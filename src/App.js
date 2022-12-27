dd13af31b9bc74b5f4b606503c1c771decbf7fd3
starydv7 <pawanstar72926@gmail.com>
2022-12-27 16:06:34 +0530

    class components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,82 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 
-import './App.css';
-import Home from './components/Home';
-import TodoApp from './components/TodoApp';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Home/> */}
-      {/* <Todo/> */}
-      <TodoApp/>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take Out Trash',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Lunch',
+        completed: true,
+      },
+      {
+        id: 3,
+        title: 'Shopping',
+        completed: false,
+      },
+    ],
+  };
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  delTodo = (id) => {
+    
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== id),
+    });
+  };
+
+  addTodo = (title) => {
+
+    const length = 32;
+
+    const newTodo = {
+      id: Math.round(
+        Math.pow(36, length + 1) - Math.random() * Math.pow(36, length)
+      )
+        .toString(36)
+        .slice(1),
+      title,
+      completed: false,
+    };
+
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+       
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        {/* </div> */}
+      </div>
+    );
+  }
 }
-
-export default App;
